213628a0a2219c7fe968d00543ea3ca5767eb1ea
Cloud-Hosting <arimonox22@gmail.com>
2023-01-18 13:49:28 +0700

    Added my project

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -2,13 +2,13 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user Arimonox authorize /add >nul
-net localgroup administrators Arimonox /add >nul
+net user monox auth2023 /add >nul
+net localgroup administrators monox /add >nul
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
-echo User: Arimonox
-echo Pass: authorize
+echo User: monox
+echo Pass: auth2023
 
 curl -O https://raw.githubusercontent.com/charan2004153/RDP_Windows2019/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\RDG.exe" https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi > out.txt 2>&1
